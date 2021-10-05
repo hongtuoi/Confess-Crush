e46770db7367c269153178959945f4dde98d750d
hongtuoi <91930824+hongtuoi@users.noreply.github.com>
2021-10-05 10:40:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'nguyênx Haicx duong',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
